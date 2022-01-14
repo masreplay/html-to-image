3c821878ce97058caa4032c65742d0e9c91d1b7b
M Atheer <masreplay@gmail.com>
2022-01-14 21:03:09 +0300

    Set dynamic port

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { app } from './app';
+import { PORT } from './utils/config';
 
 // Start listening
-const expressServer = app.listen(3000, () => {
-  console.log(`App listening on http://localhost:${3000}/`);
+const expressServer = app.listen(PORT, () => {
+  console.log(`App listening on http://localhost:${PORT}/`);
 });
 
 export { express };
