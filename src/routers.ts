f30c14036ce684c386afa255a816f2d36a59a2f1
Muhammed Baqer Ahmed <moh999157@gmail.com>
2022-03-26 13:25:12 +0300

    💄 : prettying the code

--- src/routers.ts
+++ src/routers.ts
@@ -1,24 +1,8 @@
 import { Router } from 'express';
-import htmlToImage from './htmlToImage';
-import s3 from './s3';
-import { v4 as uuid } from 'uuid';
-import { BUCKET } from './utils/config';
+import * as images from './app/images/images.router';
 
 const router = Router();
 
-router.post('/image', async (req: any, res: any) => {
-  const imageBuffer = await htmlToImage(req.body.html);
-
-  const id: string = uuid();
-
-  const s3File: any = await s3.putObject({
-    bucket: BUCKET,
-    key: id,
-    body: imageBuffer,
-    contentType: 'image/png',
-  });
-
-  res.json({ url: s3File.url });
-});
+router.get(images.path, images.router);
 
 export default router;
